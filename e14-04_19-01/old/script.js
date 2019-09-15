1c7ae2ed0f97d11a07bc63a6f96de59bfe0e21f9
Piotr Florczak <piotrflorczakk@gmail.com>
2019-09-15 20:43:45 +0200

    Hurtownia

---
+++ e14-04_19-01/old/script.js
@@ -0,0 +1,7 @@
+const inputNumber = document.querySelector("#cafeNumber");
+const inputWeight = document.querySelector("#enterWeight");
+const button = document.querySelector("button");
+
+button.addEventListener("click", e => {
+  e.preventDefault(); // Zapobiega odswierzaniu sie strony
+});
